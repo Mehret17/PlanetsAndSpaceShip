6de1833654fcea06e980d3dae7d11ecbfaa38963
Mehret Kulala <merutaye@gmail.com>
2018-09-11 01:41:44 -0500

    planets compeleted

--- PlanetsAndSpaceShips/Program.cs
+++ PlanetsAndSpaceShips/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlanetsAndSpaceShips
 {
@@ -6,7 +7,39 @@ namespace PlanetsAndSpaceShips
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            var lastTwoPlanets = new List<string>();
+            lastTwoPlanets.Add("Neptune"); 
+            lastTwoPlanets.Add("Uranius");
+
+            planetList.AddRange(lastTwoPlanets);
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            planetList.Add("Pluto");
+
+            var rockyMountains = planetList.GetRange(3, 4);
+
+            foreach(string rockyPlanet in rockyMountains )
+            {
+                Console.WriteLine(rockyPlanet);
+            }
+            Console.WriteLine();
+
+            planetList.Remove("Pluto");
+
+            foreach(string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
+           // Console.WriteLine(planetList);
+            Console.ReadLine();
+
         }
     }
 }
