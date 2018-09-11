95ac444cd0f4b699b7cc0d0028885ca305847bb5
Mehret Kulala <merutaye@gmail.com>
2018-09-11 01:10:18 -0500

    Add project files.

---
+++ PlanetsAndSpaceShips/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlanetsAndSpaceShips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
